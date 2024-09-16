e4ec9458ad587762b0c062e8709d31d7e1357baa
ArashAmery <arash411485@gmail.com>
2024-09-16 05:12:26 -0700

    Log out settings completed.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Welcome to my website!")
+console.log("Welcome to my website!");
+function logOut(){
+    userAc = confirm("You want to log out?");
+    if(userAc == true){
+        console.log("user Log Out.");
+    }
+    else{
+        console.log("user stay with us!");
+    }
+}
